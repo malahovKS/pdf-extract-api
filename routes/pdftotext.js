044cb7fdde0bd74a5323be5d8d59469b84788022
Malahov Kirill <malahovkirill@gmail.com>
2017-10-24 12:18:58 +0300

    add command-exists module function

--- routes/pdftotext.js
+++ routes/pdftotext.js
@@ -2,6 +2,7 @@ import log from "../libs/log";
 import multer from "multer";
 import fs from "fs";
 import * as PDFTOTEXT from "child_process";
+import commandExists from "command-exists";
 
 const UPLOAD = multer({dest: 'public/uploads/'}).single('pdf');
 
@@ -25,25 +26,33 @@ module.exports = app => {
 			return log.error("400 Error: File upload only supports .pdf filetype");
 		}
 
-		PDFTOTEXT.exec("pdftotext -layout -nopgbrk -raw -eol unix " + req.file.path + " -", {maxBuffer: 1000 * 1024},
-			(error, stdout, stderr) => {
 
-				if (error) {
-					fs.unlink(req.file.path);
-					res.sendStatus(500);
-					log.error(error);
-					return log.error(stderr);
-				}
+		commandExists('pdftotext')
+			.then(() => {
+				PDFTOTEXT.exec("pdftotext -layout -nopgbrk -raw -eol unix " + req.file.path + " -", {maxBuffer: 1000 * 1024},
+					(error, stdout, stderr) => {
 
-				log.info(req.file.originalname + " Done!");
-				log.debug("\n" + stdout);
+						if (error) {
+							fs.unlink(req.file.path);
+							res.sendStatus(500);
+							log.error(error);
+							return log.error(stderr);
+						}
 
-				fs.unlink(req.file.path);
-				res.type('text/html');
-				res.status(200).send(stdout.trim());
-				res.flush();
+						log.info(req.file.originalname + " Done!");
+						log.debug("\n" + stdout);
 
-			})
+						fs.unlink(req.file.path);
+						res.type('text/html');
+						res.status(200).send(stdout.trim());
+						res.flush();
+
+					})
+			}).catch(() => {
+			fs.unlink(req.file.path);
+			res.status(500).json({error: "Command pdftotext doesn't exist"});
+			return log.error("500 Error: Command pdftotext doesn't exist");
+		});
 
 
 	});
