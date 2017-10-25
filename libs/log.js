735925afa4e758acd3cfc5b13d8019a427881509
Malahov Kirill <malahovkirill@gmail.com>
2017-10-25 10:26:46 +0300

    optimize logging levels

--- libs/log.js
+++ libs/log.js
@@ -2,29 +2,26 @@ import winston from "winston";
 
 winston.emitErrs = true;
 const tsFormat = () => (new Date().toLocaleString());
-const log = new winston.Logger({
-	transports: [
-		// new winston.transports.File({
-		// 	level: 'info',
-		// 	filename: './all-logs.log',
-		// 	handleExceptions: true,
-		// 	json: true,
-		// 	maxsize: 5242880, //5MB
-		// 	maxFiles: 5,
-		// 	colorize: false
-		// }),
-		new (winston.transports.Console)({
-			colorize: true,
-			json: false,
-			prettyPrint: true,
-			handleExceptions: true,
-			timestamp: tsFormat,
-			//label: module.filename.split('/').slice(-2).join('/'),
-			level: 'debug'
-		})
-	],
-	exitOnError: false
-});
+const log = new winston.Logger({exitOnError: false});
+
+if (process.env.NODE_ENV === 'production') {
+	log.add(winston.transports.Console, {
+		colorize: true,
+		prettyPrint: true,
+		timestamp: tsFormat,
+		level: 'error'
+	});
+} else {
+	log.add(winston.transports.Console, {
+		colorize: true,
+		json: false,
+		prettyPrint: true,
+		handleExceptions: true,
+		timestamp: tsFormat,
+		//label: module.filename.split('/').slice(-2).join('/'),
+		level: 'debug'
+	});
+}
 
 module.exports = log;
 module.exports.stream = {
@@ -32,8 +29,6 @@ module.exports.stream = {
 		log.debug(message.trim());
 	}
 };
-
-//TODO If we're not in production only then log to the `console`
 
 
 
