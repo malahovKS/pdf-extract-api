c840666b80b8cf7aefc748eee7b94d6f03e3712c
Malahov Kirill <malahovkirill@gmail.com>
2017-10-25 12:33:43 +0300

    update app.get("/api/pdftotext")

--- routes/pdftotext.js
+++ routes/pdftotext.js
@@ -9,7 +9,7 @@ const UPLOAD = multer({dest: 'public/uploads/'}).single('pdf');
 module.exports = app => {
 
 	app.get("/api/pdftotext", (req, res) => {
-		res.status(200).json({status: "PDF to text/html API"});
+		return res.status(200).json({status: "PDF to text/html API"});
 
 		//TODO res.flush() (node:28258) DeprecationWarning: OutgoingMessage.flush is deprecated. Use flushHeaders instead.
 		// res.flush();
