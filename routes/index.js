d4b15627452d79f2f9c714b210222948c46ae9d8
Malahov Kirill <malahovkirill@gmail.com>
2017-10-28 12:32:12 +0300

    apidocjs init

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 module.exports = app => {
 	/**
 	 * @api {get} / API status
+	 * @apiGroup Status
 	 * @apiSuccess {String} status API status' message
 	 * @apiSuccessExample {json} Success-Response:
 	 * HTTP/1.1 200 OK
-	 *        {
-	 *			"status": "pdf-extract-api"
-	 *		}
+	 * {"status": "pdf-extract-api"}
 	 */
 	app.get("/", (req, res) => {
 		return res.status(200).json({status: "pdf-extract-api"});
