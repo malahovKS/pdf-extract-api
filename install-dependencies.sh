6877b9622a0714a01f26d495749b74f432e8a03f
Malahov Kirill <malahovkirill@gmail.com>
2017-10-30 13:45:40 +0200

    update user notifications

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,11 +2,13 @@
 
 #Check dependencies
 #Check Node
-if ! type "node" > /dev/null 2>&1; then
+if ! type "node" > /dev/null 2>&1;
+ printf "\n Node + \n"
+ then
     printf >&2 "node not found! Install? (y/n):";
     read -n1 REPLY
     case $REPLY in
-      y|Y) printf "\nInstalling Node.js v6.x . . .\n";
+      y|Y) printf "\n Installing Node.js v6.x . . . \n";
            # Using Ubuntu
             curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
             sudo apt-get install -y nodejs
@@ -15,27 +17,29 @@ if ! type "node" > /dev/null 2>&1; then
             n lts
             npm install npm@latest -g
             npm -v ;;
-      n|N) printf "\nAbort\n";
+      n|N) printf "\n Abort \n";
            exit 1 ;;
       *) exit 1 ;;
     esac
 fi
 
 #Check pdftotext
-if ! type "pdftotext" > /dev/null 2>&1; then
+if ! type "pdftotext" > /dev/null 2>&1;
+ printf "\n pdftotext + \n"
+ then
     printf >&2 "pdftotext not found! Install? (y/n):";
     read -n1 REPLY
     case $REPLY in
-      y|Y) printf "\nInstalling poppler-utils . . .\n";
+      y|Y) printf "\ nInstalling poppler-utils . . . \n";
            sudo apt-get update
            sudo apt-get upgrade
            sudo apt-get install poppler-utils ;;
-      n|N) printf "\nAbort\n";
+      n|N) printf "\n Abort \n";
            exit 1 ;;
       *) exit 1 ;;
     esac
 fi
 
 #Install node_modules
-printf "\nInstall node_modules . . .\n"
+printf "\n Install node_modules . . . \n"
 npm install
